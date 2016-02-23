332272f1d78c4b958e0e137dce0ca27bdbf1d68b
Higor Correa <higor_manoel2@hotmail.com>
2016-02-23 14:11:40 -0300

    Ordem de criação de tabelas.

--- Academia.EntityFramework/Model/AcademiaModel.Designer.cs
+++ Academia.EntityFramework/Model/AcademiaModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Higor Correa\documents\visual studio 2013\Projects\CorpoEmAcao\Academia.EntityFramework\Model\AcademiaModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\higor.correa\Source\Repos\academia\Academia.EntityFramework\Model\AcademiaModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
